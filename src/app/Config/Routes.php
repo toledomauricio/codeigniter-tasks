ca0516f84714b767918bf8126196dd2c024c0ef9
Mauricio Toledo <mauricio.toledo@digi.ag>
2023-10-10 11:46:51 -0300

    fix route group for tasks

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -8,8 +8,8 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->group('api/tasks', ['namespace' => 'App\Controllers'], function ($routes) {
-    $routes->get('/', 'TasksApi::index');
-    $routes->post('/', 'TasksApi::create');
-    $routes->put('update/(:num)', 'TasksApi::update/$1');
-    $routes->delete('delete/(:num)', 'TasksApi::delete/$1');
+    $routes->get('/', 'TasksApiController::index');
+    $routes->post('/', 'TasksApiController::create');
+    $routes->put('update/(:num)', 'TasksApiController::update/$1');
+    $routes->delete('delete/(:num)', 'TasksApiController::delete/$1');
 });
