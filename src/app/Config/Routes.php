4bf999fb9cb29d23f5617977d55c7ebfbbaaaf67
Mauricio Toledo <mauricio.toledo@digi.ag>
2023-10-10 11:46:30 -0300

    add route group for tasks

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api/tasks', ['namespace' => 'App\Controllers'], function ($routes) {
+    $routes->get('/', 'TasksApi::index');
+    $routes->post('/', 'TasksApi::create');
+    $routes->put('update/(:num)', 'TasksApi::update/$1');
+    $routes->delete('delete/(:num)', 'TasksApi::delete/$1');
+});
