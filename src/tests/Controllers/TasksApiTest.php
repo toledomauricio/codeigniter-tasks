3705809a085fc133b11751c3440f07a8a7ce85ce
Mauricio Toledo <mauricio.toledo@digi.ag>
2023-10-10 12:20:54 -0300

    add test coverage for create tasks

--- src/tests/Controllers/TasksApiTest.php
+++ src/tests/Controllers/TasksApiTest.php
@@ -13,4 +13,28 @@ class TasksApiTest extends FeatureTestCase
         $result = $this->get('api/tasks');
         $result->assertStatus(200);
     }
+
+    public function testCreate()
+    {
+        // Realiza um teste para a rota de criação (POST) de tarefas
+        $data = [
+            'title' => 'Tarefa de teste ' . rand(5, 15),
+            'description' => 'Descrição de teste ' . rand(5, 15),
+            'status' => 'pending',
+        ];
+
+        $json = json_encode($data);
+
+        $result = $this->withHeaders(['Content-Type' => 'application/json'])
+            ->withBody($json)
+            ->post('/api/tasks');
+
+        $result->assertStatus(201);
+        $responseData = $result->getJson();
+        $dataDecoded = json_decode($responseData);
+
+        $this->assertSame($data['title'], $dataDecoded->title);
+        $this->assertSame($data['description'], $dataDecoded->description);
+        $this->assertSame($data['status'], $dataDecoded->status);
+    }
 }
