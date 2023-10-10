2ab968ad4996449baf0716ba6dfd33f68db5c8a3
Mauricio Toledo <mauricio.toledo@digi.ag>
2023-10-10 11:52:01 -0300

    fix database configuration for docker

--- src/app/Config/Database.php
+++ src/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'db',
+        'username'     => 'root',
+        'password'     => 'codeigniter@',
+        'database'     => 'test',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
