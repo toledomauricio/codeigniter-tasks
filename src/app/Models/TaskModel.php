d30fd17ab33674ffdd1be17a3afe738de3514622
Mauricio Toledo <mauricio.toledo@digi.ag>
2023-10-10 11:28:36 -0300

    add validationRules & validationMessages on TaskModel.php

--- src/app/Models/TaskModel.php
+++ src/app/Models/TaskModel.php
@@ -12,4 +12,26 @@ class TaskModel extends Model
     protected $allowedFields = ['title', 'description', 'status'];
 
     protected $returnType = 'object';
+    // protected $useTimestamps = true;
+
+    // protected $dateFormat = 'datetime';
+
+    // protected $createdField = 'created_at';
+    // protected $updatedField = 'updated_at';
+
+    protected $validationRules = [
+        'title' => 'required|max_length[255]',
+        'description' => 'permit_empty',
+    ];
+
+    protected $validationMessages = [
+        'title' => [
+            'required' => 'O campo título é obrigatório.',
+            'max_length' => 'O campo título não pode ter mais de 255 caracteres.'
+        ],
+        'status' => [
+            'required' => 'O campo status é obrigatório.',
+            'enum_string' => 'O campo status deve ser "pending", "done" ou "canceled".'
+        ]
+    ];
 }
